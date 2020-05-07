2026a08351a7c51a6eda40d8d9bec336cc8b2bc7
GianisTsol <giannisetsolakis@gmail.com>
2020-05-07 23:24:51 +0300

    Now works as intended, all basic functions implemented.

--- node.py
+++ node.py
@@ -11,22 +11,7 @@ peers = ['192.168.1.6']
 node1 = None
 sys.path.insert(0, '.')
 
-
-def data_handler(data):
-    global peers
-    dta = data
-    #dta = {'test': 'lol'}
-    dta = json.loads(data)
-    if "peers" in dta:
-        new = [i for i in peers if i not in dta["peers"]]
-        peers = dta["peers"] + new
-        print("new neighbours: " + str(new))
-    elif "msg" in dta:
-        print("msg: " + dta["msg"])
-
-
 def message(dict):
-    #print(dict)
     buf = json.dumps(dict)
     return buf
 
@@ -36,12 +21,24 @@ def peers_packet():
     buf = json.dumps(buf)
     return buf
 
+def send_peers():
+    node.send_to_nodes(peers_packet())
+
+def data_handler(data):
+    global peers
+    dta = {'test': 'lol'}
+    dta = json.loads(data)
+    if "peers" in dta:
+        new = [i for i in peers if i not in dta["peers"]]
+        peers = dta["peers"] + new
+        print("new neighbours: " + str(new))
+    elif "msg" in dta:
+        print("msg: " + dta["msg"])
+        node.send_to_nodes(dta)
 
 def node_callback(event, node, other, data):
     if (event == 'node_request_to_stop'):
         print("node request to stop")
-    #if event != 'node_request_to_stop': # node_request_to_stop does not have any connected_node, while it is the main_node that is stopping!
-    #    print('Event: {} from main node {}: connected node {}: {}'.format(event, node.id, other.id, data))
     elif ( event == "inbound_node_disconnected" ):
         print("NODE (" + node.getName() + "): " + "event:" + event + "\n")
     elif ( event == "outbound_node_disconnected" ):
@@ -49,43 +46,22 @@ def node_callback(event, node, other, data):
     elif ( event == "outbound_node_connected" ):
         print("NODE (" + node.getName() + "): " + "event:" + event + "\n")
     elif ( event == "inbound_node_connected" ):
+        send_peers()
         print("NODE (" + node.getName() + "): " + "event:" + event + "\n")
     elif ( event == "node_message" ):
-        #print("NODE (" + node.getName() + "): " + "event:" + event + ": " + "data: " + str(data) + "\n")
-        #print(data)
         data_handler(data.encode('utf-8'))
     else:
         print("NODE (" + node.getName() + "): Event is not known " + event + "\n")
 
-
-# Just for test we spin off multiple nodes, however it is more likely that these nodes are running
-# on computers on the Internet! Otherwise we do not have any peer2peer application.
-node_1 = Node("127.0.0.1", 8001, node_callback)
-node_2 = Node("127.0.0.1", 8002, node_callback)
-node_3 = Node("127.0.0.1", 8003, node_callback)
-
-
-node_1.start()
-node_2.start()
-node_3.start()
-time.sleep(1)
-
-node_1.connect_with_node('127.0.0.1', 8002)
-node_2.connect_with_node('127.0.0.1', 8003)
-node_3.connect_with_node('127.0.0.1', 8001)
-
+node = Node("", 65432, node_callback)
+node.start()
 time.sleep(1)
 
 while 1:
-    buf = str(message({'msg': "lloolll"}))
-    node_2.send_to_nodes(buf)
-    node_2.send_to_nodes(peers_packet())
-    #node_1.send_to_nodes("lolllllllllllllll")
-    time.sleep(1)
+    buf = str(message({'msg': "test123"}))
+    node.send_to_nodes(buf)
+    time.sleep(2)
 time.sleep(5)
 
-node_1.stop()
-node_2.stop()
-node_3.stop()
-
+node.stop()
 print('end')
