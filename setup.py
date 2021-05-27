0715345d5ff2f2ee77ea61dc6cccd47d288c7d9c
GianisTsol <giannisetsolakis@gmail.com>
2021-05-27 23:26:18 +0300

    License issue.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "pythonp2p"},
