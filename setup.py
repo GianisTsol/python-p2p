24cfc0280016f9372d0348ade37b63bd54e5d588
G Tsol <35958401+GianisTsol@users.noreply.github.com>
2021-05-28 15:59:20 +0300

    Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pythonp2p",
-    version="1.5",
+    version="1.6",
     author="GianisTsol",
     author_email="giannisetsolakis@gmail.com",
     description="A peer to peer network able to transfer data and files",
